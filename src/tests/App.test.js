a5ef39b57cc10706a9f982c4d8001ce6dd9ef337
Henryck <ryck302@gmail.com>
2022-07-20 16:08:08 -0300

    requisito 01 - completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
@@ -13,5 +14,43 @@ describe('testar component App', () => {
     expect(homeLink).toBeInTheDocument();
     expect(aboutLink).toBeInTheDocument();
     expect(favoriteLink).toBeInTheDocument();
+  });
+
+  it('é direcionado para a página inicial ao clicar no link home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('é direcionado para a página about ao clicar no link about', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('é direcionado para a página favoritos ao clicar no link favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('é direcionado para a página Not Found ao inserir uma Url desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-que-nao-existe');
+
+    const notFoundTitle = screen.getByText('Page requested not found');
+    expect(notFoundTitle).toBeInTheDocument();
   });
 });
