cb28f3b99b14d192915b2e9159cee0ef312bf0bf
Henryck <ryck302@gmail.com>
2022-07-20 16:54:45 -0300

    requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import About from '../pages/About';
+
+describe('testar component About', () => {
+  it('a página contém um heading h2 com o texto about pokedex', () => {
+    renderWithRouter(<About />);
+    const headTitle = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(headTitle).toBeInTheDocument();
+  });
+
+  it('a página contém dois parágrafos com texto sobre a pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraphs = screen.getAllByText(/Pokémons/);
+    expect(paragraphs).toHaveLength(2);
+  });
+
+  it('a página contém uma image de uma pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexImage = screen.getByRole('img');
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(pokedexImage).toBeInTheDocument();
+    expect(pokedexImage).toHaveAttribute('src', src);
+  });
+});
