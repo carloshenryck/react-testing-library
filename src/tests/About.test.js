02087be0449e27961c0979f4a7ada1996d756e53
Henryck <ryck302@gmail.com>
2022-07-20 18:16:29 -0300

    requisito 04

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -16,7 +16,7 @@ describe('testar component About', () => {
     expect(paragraphs).toHaveLength(2);
   });
 
-  it('a página contém uma image de uma pokédex', () => {
+  it('a página contém uma imagem de uma pokédex', () => {
     renderWithRouter(<About />);
     const pokedexImage = screen.getByRole('img');
     const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('testar component notFound', () => {
+  it('a página contém um heading h2', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundtitle = screen.getByRole('heading', {
+      name: /Page requested not found/, level: 2,
+    });
+
+    expect(notFoundtitle).toBeInTheDocument();
+  });
+
+  it('a página contém um gif', () => {
+    renderWithRouter(<NotFound />);
+    const pikachuGif = screen.getByAltText(/Pikachu crying/);
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(pikachuGif).toBeInTheDocument();
+    expect(pikachuGif).toHaveAttribute('src', src);
+  });
+});
